3f1783ee10d7a81207bd39ebe55329b6e237cdd6
Adam Parrish <aparrish@neosavvy.com>
2013-11-09 15:26:09 -0500

    Reformatting so I can read this.

--- app/com/currant/model/package.scala
+++ app/com/currant/model/package.scala
@@ -2,15 +2,39 @@ package com.currant
 
 package object model {
 
-  case class SportCreateRequest(name : String, description : String, active : Boolean, imageUrl : Option[String] = None, minPlayers : Option[Int] = None, maxPlayers : Option[Int] = None, waitList : Option[Int] = None)
+  case class SportCreateRequest(
+                                 name: String,
+                                 description: String,
+                                 active: Boolean,
+                                 imageUrl: Option[String] = None,
+                                 minPlayers: Option[Int] = None,
+                                 maxPlayers: Option[Int] = None,
+                                 waitList: Option[Int] = None)
 
-  case class Sport(id : Long, name : String, description : String, active : Boolean, imageUrl : Option[String], minPlayers : Option[Int], maxPlayers : Option[Int], waitList : Option[Int])
+  case class Sport(
+                    id: Long,
+                    name: String,
+                    description: String,
+                    active: Boolean,
+                    imageUrl: Option[String],
+                    minPlayers: Option[Int],
+                    maxPlayers: Option[Int],
+                    waitList: Option[Int])
 
-/*  case class SportUpdateRequest()*/
+  /*  case class SportUpdateRequest()*/
 
-  case class EquipmentCreateRequest(name : String, description : String, active : Boolean, sports : Seq[Long], imageUrl : Option[String])
+  case class EquipmentCreateRequest(
+                                     name: String,
+                                     description: String,
+                                     active: Boolean,
+                                     sports: Seq[Long],
+                                     imageUrl: Option[String])
 
-  case class Equipment(id : Long, name : String, description : String, active : Boolean, sports : Seq[Long], imageUrl : Option[String])
+  case class Equipment(
+                        id: Long, name: String,
+                        description: String, active: Boolean,
+                        sports: Seq[Long],
+                        imageUrl: Option[String])
 
 
 }
