d9020dcd647a1c7a4f6a43b696ba43c8b2907248
UnicodeSnowman <utfsnowman@gmail.com>
2013-11-14 19:34:42 -0500

    using CurrantController

--- app/com/currant/club/ClubsController.scala
+++ app/com/currant/club/ClubsController.scala
@@ -1,8 +1,9 @@
 package com.currant.club
 
+import com.currant.base.CurrantController
 import play.api.mvc._
 
-object ClubsController extends Controller {
+object ClubsController extends CurrantController {
   def create() = play.mvc.Results.TODO
 
   def findMembers( clubId: Float ) = play.mvc.Results.TODO
